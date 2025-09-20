1553644ea2e2cad4cc68c5969b36ce78be038456
Atul Srivastava <iatulsrivastav@gmail.com>
2025-09-20 06:57:28 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -7,6 +7,7 @@ const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
         origin:"http://localhost:5173",
+          "https://hexagram-nsf9.onrender.com", // ✅ your deployed frontend
         methods:["GET","POST"]
     }
 })
