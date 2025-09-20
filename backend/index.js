99b53a2131fa483429dd3da0165a3063aab6af2f
Atul Srivastava <iatulsrivastav@gmail.com>
2025-09-20 06:33:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
-    "https://hexagram-nsf9.onrender.com"
+    origin:"http://localhost:5173"
     credentials:true
 }))
 app.use(express.json())
